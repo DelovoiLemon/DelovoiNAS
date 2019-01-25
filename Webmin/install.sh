295301462dc25c97bbf7b75f4b04d9974d9e3023
DelovoiLemon <47033417+DelovoiLemon@users.noreply.github.com>
2019-01-25 19:00:07 +0300

    Webmin installiation script

---
+++ Webmin/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl libdigest-md5-perl
+echo 'Installing SSH tools...'
+sudo apt-get install openssh-server
+echo 'Installing SSH tools complite!'
+sudo echo 'deb http://download.webmin.com/download/repository sarge contrib'>>/etc/apt/sources.list
+sudo echo 'deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib'>>/etc/apt/sources.list
+rm jcameron-key.asc>/dev/null
+wget http://www.webmin.com/jcameron-key.asc>/dev/null
+sudo apt-key add jcameron-key.asc>/dev/null
+sudo apt update -y>/dev/null
+sudo apt-get install webmin>/dev/null
+sudo systemctl enable webmin>/dev/null
