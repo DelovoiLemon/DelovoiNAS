3bce4111bf1e9baa9013433b0ca603b16259a9e2
DelovoiLemon <47033417+DelovoiLemon@users.noreply.github.com>
2019-01-25 19:04:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,22 +12,18 @@ sudo ./install.sh
 cd ..
 echo 'Samba setup complite!'
 echo 'Starting transmission-daemon setup...'
-sudo apt-get install -y transmission-daemon>/dev/null
-sudo service transmission-daemon stop>/dev/null
-sudo cp ~/DelovoiNAS/transmission-daemon/settings.json /etc/transmission-daemon/settings.json>/dev/null
-sudo service transmission-daemon start>/dev/null
+cd transmission-daemon
+./install.sh
+cd ..
 echo 'Transmission-daemon setup complite!'
 echo 'Webmin installing starting...'
-sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl libdigest-md5-perl
-echo 'Installing SSH tools...'
-sudo apt-get install openssh-server
-echo 'Installing SSH tools complite!'
 cd Webmin/
 ./install.sh
 cd ..
 echo 'Webmin install complite!'
 echo 'Plex installing starting...'
-cd ~/DelovoiNAS/Serviio/
+cd Plex
 ./install.sh
+cd ..
 echo 'Plex install complite!'
 echo 'Instaliation complite!'
