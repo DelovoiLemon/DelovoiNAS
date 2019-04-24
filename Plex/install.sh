94475da25ba57007662584ae78ceb9cbc4320ecf
DelovoiLemon <47033417+DelovoiLemon@users.noreply.github.com>
2019-04-24 19:44:48 +0300

    Update install.sh

--- Plex/install.sh
+++ Plex/install.sh
@@ -2,8 +2,8 @@
 sudo apt-get update -y>/dev/null
 sudo apt-get upgrade -y>/dev/null
 cd ~/DelovoiNAS/Plex
-wget https://downloads.plex.tv/plex-media-server/1.14.1.5488-cc260c476/plexmediaserver_1.14.1.5488-cc260c476_amd64.deb
-mv plexmediaserver_1.14.1.5488-cc260c476_amd64.deb Plex.deb
+wget https://downloads.plex.tv/plex-media-server-new/1.15.4.993-bb4a2cb6c/debian/plexmediaserver_1.15.4.993-bb4a2cb6c_armhf.deb
+mv plex*.deb Plex.deb
 sudo dpkg -i Plex.deb>/dev/null
 sudo systemctl start plexmediaserver>/dev/null
 sudo systemctl enable plexmediaserver>/dev/null
