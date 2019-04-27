be4772ef87394f7c417d30c5dfaf8c1220abc407
DelovoiLemon <47033417+DelovoiLemon@users.noreply.github.com>
2019-04-27 22:29:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,19 +26,32 @@ cd Samba
 sudo ./install.sh
 cd ..
 echo 'Samba setup complite!'
-echo 'Starting transmission-daemon setup...'
-cd transmission-daemon
-./install.sh
-cd ..
-echo 'Transmission-daemon setup complite!'
-echo 'Webmin installing starting...'
-cd Webmin/
-./install.sh
-cd ..
-echo 'Webmin install complite!'
-echo 'Plex installing starting...'
-cd Plex
-./install.sh
-cd ..
-echo 'Plex install complite!'
+#echo 'Starting transmission-daemon setup...'
+#cd transmission-daemon
+#./install.sh
+#cd ..
+#echo 'Transmission-daemon setup complite!'
+#echo 'Webmin installing starting...'
+#cd Webmin/
+#./install.sh
+#cd ..
+#echo 'Webmin install complite!'
+#echo 'Plex installing starting...'
+#cd Plex
+#./install.sh
+#cd ..
+#echo 'Plex install complite!'
+echo 'Ajenti installing starting..'
+sudo service apache2 stop
+sudo apt-get purge apache2 apache2-utils apache2-bin apache2-common
+sudo apt-get autoremove --purge
+sudo rm -Rf /etc/apache2 /usr/lib/apache2 /usr/include/apache2
+sudo -s
+wget -O- https://raw.github.com/ajenti/ajenti/1.x/scripts/install-debian.sh | sh
+service ajenti restart
+apt-get install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-ftp-pureftpd ajenti-v-php-fpm php7-curl
+service ajenti restart
+apt-get install php5-mysql imagemagick php5-imagick php5-gd
+service ajenti restart
 echo 'Instaliation complite!'
+sudo reboot
